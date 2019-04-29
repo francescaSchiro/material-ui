114b8b1f997089f35e0797c3b434ed3f2ce8431f
Schiro Francesca <francesca.schiro@sisal.it>
2019-04-29 17:20:41 +0200

    ADD meta | CssBaseLine | Material Button

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+----
+
+[Setting up Material UI](https://material-ui.com/getting-started/installation/)
+
+----
+
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Button from '@material-ui/core/Button';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </header>
     </div>
   );
 }
+// import logo from './logo.svg';
 
 export default App;
+
+      //   <img src={logo} className="App-logo" alt="logo" />
+      //   <p>
+      //     Edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      //   <a
+      //     className="App-link"
+      //     href="https://reactjs.org"
+      //     target="_blank"
+      //     rel="noopener noreferrer"
+      //   >
+      //     Learn React
+      //   </a>
