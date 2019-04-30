b308e942cc14c408cd2bcfcc7f0f5f6c75d0c811
Schiro Francesca <francesca.schiro@sisal.it>
2019-04-30 11:07:05 +0200

    ADD NavBar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React from 'react';
 
-import Button from '@material-ui/core/Button';
-
-import './App.css';
+import Button from './components/Button';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
+      <header >
+        <NavBar />
+        <Button label={'ciao'} />
+
       </header>
     </div>
   );
 }
-// import logo from './logo.svg';
 
 export default App;
-
-      //   <img src={logo} className="App-logo" alt="logo" />
-      //   <p>
-      //     Edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      //   <a
-      //     className="App-link"
-      //     href="https://reactjs.org"
-      //     target="_blank"
-      //     rel="noopener noreferrer"
-      //   >
-      //     Learn React
-      //   </a>
---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { ButtonStyled } from './styles';
+
+
+export default function Button({ label }) {
+    return (
+        <ButtonStyled label={label} />
+    );
+};
